996dbabaf622f68be7af800800869acadd024d3d
Kuvuklija <43697762+Kuvuklija@users.noreply.github.com>
2019-02-14 16:03:10 +0300

    Initial commit

---
+++ WebServices/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using WebServices.Models;
+
+namespace WebServices.Controllers{
+
+    public class HomeController : Controller{
+
+        //  ---MVC---
+        //private ReservationRepository repo = ReservationRepository.Current;
+    
+        //public ActionResult Index(){
+        //    return View(repo.GetAll());
+        //}
+
+        //public ActionResult Add(Reservation item) {
+        //    if (ModelState.IsValid) {
+        //        repo.Add(item);
+        //        return RedirectToAction("Index");
+        //    } else {
+        //        return View("Index");
+        //    }
+        //}
+
+        //public ActionResult Remove(int id) {
+        //    repo.Remove(id);
+        //    return RedirectToAction("Index");
+        //}
+
+        //public ActionResult Update(Reservation item) {
+        //    if(ModelState.IsValid && repo.Update(item)) {
+        //        return RedirectToAction("Index");
+        //    } else {
+        //        return View("Index");
+        //    }
+        //}
+
+        public ViewResult Index() {
+            return View(); //only html-markup, model is used by API controller
+        }
+    }
+}
